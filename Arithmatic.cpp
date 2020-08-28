fa10c62535b00e78b50941bf5f810b2e33dbd9e4
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-08-28 16:51:45 +0530

    Added Arithmatic.cpp program

---
+++ Arithmatic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    float a,b,c;
+    cout<<"Enter a: ";
+    cin>>a;
+    cout<<"\nEnter b: ";
+    cin>>b;
+    cout<<"\nAddition: "<<a+b;
+    cout<<"\nSubtraction: "<<a-b;
+    cout<<"\nMultiplication: "<<a*b;
+    c=a/b;
+    cout<<"\nDivision: "<<c;
+    return 0;
+}
