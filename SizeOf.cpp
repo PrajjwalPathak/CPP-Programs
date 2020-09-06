72beea5a5449e32291fdf8f79b563b71ef98bbbd
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-09-06 18:02:45 +0530

    Added SizeOf.cpp program

---
+++ SizeOf.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a;
+    float b;
+    double c;
+    char d;
+    cout<<"Size of int: "<<sizeof(a);
+    cout<<"\nSize of float: "<<sizeof(b);
+    cout<<"\nSize of double: "<<sizeof(c);
+    cout<<"\nSize of char: "<<sizeof(d);
+
+
+    return 0;
+}
