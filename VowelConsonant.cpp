9fe9732e90da06a339e3276207820573a0aadb72
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-11-14 12:35:18 +0530

    Update VowelConsonant.cpp

--- VowelConsonant.cpp
+++ VowelConsonant.cpp
@@ -1,15 +1,16 @@
+//Program to check if a character is a Vowel or a Consonant
+
 #include<iostream>
 using namespace std;
 int main() {
     char a;
-    cout<<"Enter the character to check if Vowel or Consonent: ";
+    cout<<"Enter the character to check if Vowel or Consonant: ";
     cin>>a;
     if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U') {
         cout<<"Vowel";
     }
     else {
-        cout<<"Consonent";
+        cout<<"Consonant";
     }
-    
     return 0;
 }
