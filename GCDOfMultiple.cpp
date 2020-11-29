4ca2f35511769a4e5ccfb2622068e45f5f086974
Prajjwal Pathak <prajjwalpathak.asm@gmail.com>
2020-11-29 20:13:02 +0530

    Add GCDOfMultiple.cpp

---
+++ GCDOfMultiple.cpp
@@ -0,0 +1,34 @@
+//Program to find the GCD of multiple numbers
+
+#include<iostream>
+
+using namespace std;
+
+int gcd(int a, int b) 
+{ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+}
+
+int gcdmultiple(int a[], int n) {
+    int i;
+    for(i=0;i<n-1;i++) {
+        a[i+1] = gcd(a[i],a[i+1]);
+    }
+    return a[n-1];
+}
+
+int main() {
+    int i,n,hcf;
+    cout<<"Enter n: ";
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    hcf=gcdmultiple(a,n);
+    cout<<"HCF = "<<hcf;
+    
+    return 0;
+}
