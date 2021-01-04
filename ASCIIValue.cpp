ad560b84a9c18577b344410da6292af2b8247748
prajjwalpathak <prajjwalpathak.asm@gmail.com>
2021-01-04 20:29:52 +0530

    Add ASCIIValue.cpp

---
+++ ASCIIValue.cpp
@@ -0,0 +1,14 @@
+//Program to find ASCII value of character
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    char i;
+    cout<<"Enter a character: ";
+    cin>>i;
+    cout<<"The ASCII value is: "<<(int)i;
+    
+    return 0;
+}
