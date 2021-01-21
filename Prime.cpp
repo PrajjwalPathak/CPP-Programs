a449abcad85b1787928b1575733af1b5697b0e6e
prajjwalpathak <prajjwalpathak.asm@gmail.com>
2021-01-21 21:21:54 +0530

    Add Prime.cpp

---
+++ Prime.cpp
@@ -0,0 +1,33 @@
+//Program to find if a number is prime or not
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int i,n,flag=0;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(n==0 || n==1) {
+        cout<<"Not a Prime";
+    }
+    else if(n==2 || n==3) {
+        cout<<"Prime";
+    }
+    else {
+        for(i=2;i<=n/2;i++) {
+            if(n%i==0) {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0) {
+            cout<<"Prime";
+        }
+        else {
+            cout<<"Not a Prime";
+        }
+    }
+    
+    return 0;
+}
